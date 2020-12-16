58060dc912e9b1a49726344ab5a72069b0804ee9
Steven <osteve964@gmail.com>
2020-12-16 07:58:41 +0300

    deploy my react app to github pages

--- src/App.js
+++ src/App.js
@@ -1,24 +1,45 @@
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
+import chuck from './chk.jpg';
+import axios from 'axios';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+   
+   const [state, setState] = useState({
+     joke: ''
+   })
+ useEffect(  () => {
+   fetchData();
+    }, []);
+
+ const fetchData = async ()  => {
+  const result = await axios.get('https://api.chucknorris.io/jokes/random');
+  console.log(result.data.value);
+  setState({
+    ...state,
+    joke: result.data.value
+  })
+ }
+
+ return (
+ <div className = "container">
+     <h1 className = "title">Chuck Norris</h1>
+     <div className = "feature_one">
+      <h3 className = "title">Welcome now take a spin and Let's Find a Legendary Jokes</h3>
     </div>
+    <div className = "feature_two">
+      <div className = "feature_three">
+        <img src ={chuck} alt = "legends"/>
+      </div>
+      <div>
+      <button className = "btn" type = "submit" onClick = { fetchData  } >Find the Joke.</button>
+      </div>
+    </div>
+    <div className = "joke">
+    <h2 className = "title_2">And the Joke</h2>
+  <h4>Hear This {state.joke}</h4>
+    </div>
+</div>
   );
 }
 
